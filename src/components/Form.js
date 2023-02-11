7d7b7d3e0b17ca852a55a865f55284df87f0b99f
KonthamRani <rani.kontham222@gmail.com>
2023-02-12 00:19:02 +0530

    fixed error

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,21 +9,21 @@ const Form = () => {
     const handleButton = (action) => {
         console.log(typeof (parseInt(num1)), typeof (num2))
         if (num1 === "" || num2 === "") {
-            document.getElementById("result").style.display="hidden";
-            document.getElementsByClassName("success").style.display="hidden";
-            document.getElementsByClassName("failure").style.display="block";
             num1 === "" ? setFailureMessage("Error: Num1 cannot be empty") : setFailureMessage("Error: Num2 cannot be empty");
+            setSuccessMessage("")
+        }
+        else if(isNaN(num1) && isNaN(num2)){
+            setFailureMessage("Only numbers are allowed")
+            setSuccessMessage("")
         }
         else if (!isNaN(num1) && !isNaN(num2)) {
             if (action === "+") setResult(parseInt(num1) + parseInt(num2))
             else if (action === "-") setResult(num1 - num2)
             else if (action === "*") setResult(num1 * num2)
             else if (action === "/") setResult(num1 / num2)
-            document.getElementById("result").style.display="block";
-            document.getElementById("result").innerText="Result"
-            document.getElementsByClassName("success").style.display="block";
-            document.getElementsByClassName("failure").style.display="hidden";
+            document.getElementById("result").innerText="Result :";
             setSuccessMessage("Success: Your result is shown above!")
+            setFailureMessage("")
         }
 
     }
@@ -38,9 +38,9 @@ const Form = () => {
                 <h1>React Calculator</h1>
                 <input type="text" name='num1' placeholder='num1' onChange={handleChange} />
                 <input type="text" name='num2' placeholder='num2' onChange={handleChange} />
+                <h1 id="result">{result}</h1>
                 <h2 className='success'>{successMessage}</h2>
                 <h2 className='failure'>{failureMessage}</h2>
-                <h1 id="result">{result}</h1>
             </div>
             <div className='Buttons'>
                 <button onClick={() => { handleButton("+") }}>+</button>
