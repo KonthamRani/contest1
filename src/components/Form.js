45a582639d588b10aaef37c3d3516731a5f0c136
KonthamRani <rani.kontham222@gmail.com>
2023-02-11 23:57:27 +0530

    fixed style

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../components/Form.css'
+import '../components/Form.css'
 const Form = () => {
     const [num1, setNum1] = useState("");
     const [num2, setNum2] = useState("");
